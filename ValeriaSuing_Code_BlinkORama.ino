89e42f0913246e922e982da898bf699230b9a16c
valeriasuing <96031632+valeriasuing@users.noreply.github.com>
2022-02-14 19:00:28 -0500

    Add files via upload

---
+++ ValeriaSuing_Code_BlinkORama.ino
@@ -0,0 +1,56 @@
+/*
+  Valeria Suing (3182717)
+  Blink O Rama
+  Controlling fairy lights in a garment with different buttons 
+*/
+
+// setting pin numbers
+const int ledPin =  1;        // fairy lights A7
+const int buttonPin1 = 2;     // the number of the button pin A5
+const int buttonPin2 = 6;     // the number of the button pin A1   
+const int buttonPin3 = 3;     // the number of the button pin A4
+
+// variables will change:
+int buttonState1 = 0;         // variable for reading the button1 status
+int buttonState2 = 0;         // variable for reading the button2 status
+int buttonState3 = 0;         // variable for reading the button3 status
+
+void setup() {
+  // initialize the LEDs pin as an output:
+  pinMode(ledPin, OUTPUT);
+
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin1, INPUT);
+  pinMode(buttonPin2, INPUT);
+  pinMode(buttonPin3, INPUT);
+}
+
+void loop() {
+  // read the state of the button value:
+  buttonState1 = digitalRead(buttonPin1);
+  buttonState2 = digitalRead(buttonPin2);
+  buttonState3 = digitalRead(buttonPin3); 
+
+  // if buttoned up 
+  if (buttonState1 == HIGH) {
+    // Turn on LEDs
+    digitalWrite(ledPin, HIGH);
+  } 
+  
+  //if button is pressed 
+   if (buttonState2 == HIGH) {
+    //turn off LEDs
+    digitalWrite(ledPin, LOW);
+  } 
+
+  if (buttonState3 == HIGH) {
+    // turn LEDs off
+    digitalWrite(ledPin, HIGH);
+    delay(120);
+    digitalWrite(ledPin, LOW);
+    delay(120);
+    
+  } 
+  
+
+}
